da68d49003fb1465830a0473baa473b22f541e68
t-848583 <4625079@ed.tus.ac.jp>
2025-07-02 19:13:10 +0900

    00

--- astro.config.ts
+++ astro.config.ts
@@ -2,6 +2,6 @@ import { defineConfig } from 'astro/config';
 
 // https://astro.build/config
 export default defineConfig({
-   site: 'https://s-union.github.io', // https://<あなたのGitHubのユーザー名>.github.io
-   base: 'astro-hands-on', // GitHubでのレポジトリ名
+   site: 'https://t-848583.github.io', // https://<あなたのGitHubのユーザー名>.github.io
+   base: 'ridaisai0611', // GitHubでのレポジトリ名
 });
